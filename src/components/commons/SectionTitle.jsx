1dd478c158bb3be15e2d1246b3b34d918cbaccfd
elhamakbarnejad <elham813@gmail.com>
2026-01-30 11:05:55 +0330

    Modify CTAaction at SectionTitle

--- src/components/commons/SectionTitle.jsx
+++ src/components/commons/SectionTitle.jsx
@@ -1,52 +1,29 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 
-const SectionTitle = ({ title }) => {
+const CTAactionUsers = ({ path, label, loadingLabel }) => {
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
 
-  const [isDirection, setIsDirection] = useState(false);
-  const [isDirectionU, setIsDirectionU] = useState(false);
-  const toggle = () => {
-    setIsDirection(!isDirection);
+  const clickHandlerUsers = () => {
+    setLoading(true);
+    setTimeout(() => {
+      navigate(path);
+    }, 2000);
   };
-  const toggleU = () => {
-    setIsDirectionU(!isDirectionU);
-  };
+  return (
+    <button
+      onClick={clickHandlerUsers}
+      disabled={loading}
+      className="px-3 py-2 primary-bg text-white rounded-md text-sm cursor-pointer hover:opacity-80 transition-all duration-150"
+    >
+      {loading ? loadingLabel : label}
+    </button>
+  );
+};
 
-  const CTAactionUsers = () => {
-    const clickHandlerUsers = () => {
-      toggleU();
-      setTimeout(() => {
-        navigate("/users");
-      }, 2000);
-    };
-    return (
-      <button
-        onClick={clickHandlerUsers}
-        className="px-3 py-2 primary-bg text-white rounded-md text-sm cursor-pointer hover:opacity-80 transition-all duration-150"
-      >
-        {isDirectionU ? "در حال انتقال ..." : "صفحه کاربران"}
-      </button>
-    );
-  };
-
-  const CTAactionProducts = () => {
-    const clickHandler = () => {
-      toggle();
-      setTimeout(() => {
-        navigate("/products");
-      }, 2000);
-    };
-    return (
-      <button
-        onClick={clickHandler}
-        className="px-3 py-2 primary-bg text-white rounded-md text-sm cursor-pointer hover:opacity-80 transition-all duration-150"
-      >
-        {isDirection ? "در حال انتقال ..." : "صفحه محصولات"}
-      </button>
-    );
-  };
-
+const SectionTitle = ({ title }) => {
   return (
     <div className=" flex items-center justify-between px-5 py-5 shadow border primary-border-color">
       <div>
@@ -55,10 +32,18 @@ const SectionTitle = ({ title }) => {
       <div className="flex items-center justify-center w-64  gap-2">
         {" "}
         <div className="flex items-center gap-3">
-          <CTAactionUsers />
+          <CTAactionUsers
+            label="صفحه محصولات"
+            path="/products"
+            loadingLabel="در حال انتقال ..."
+          />
         </div>
         <div className="flex items-center gap-3">
-          <CTAactionProducts />
+          <CTAactionUsers
+            label="صفحه کاربران"
+            path="/users"
+            loadingLabel="در حال انتقال ..."
+          />
         </div>
       </div>
     </div>
@@ -66,3 +51,51 @@ const SectionTitle = ({ title }) => {
 };
 
 export default SectionTitle;
+
+// import { useState } from "react";
+// import { useNavigate } from "react-router";
+
+// const CTAButton = ({ label, loadingLabel, path }) => {
+//   const navigate = useNavigate();
+//   const [loading, setLoading] = useState(false);
+
+//   const clickHandler = () => {
+//     setLoading(true);
+//     setTimeout(() => {
+//       navigate(path);
+//     }, 2000);
+//   };
+
+//   return (
+//     <button
+//       onClick={clickHandler}
+//       className="px-3 py-2 primary-bg text-white rounded-md text-sm cursor-pointer hover:opacity-80 transition-all duration-150"
+//       disabled={loading}
+//     >
+//       {loading ? loadingLabel : label}
+//     </button>
+//   );
+// };
+
+// const SectionTitle = ({ title }) => {
+//   return (
+//     <div className="flex items-center justify-between px-5 py-5 shadow border primary-border-color">
+//       <h2 className="text-2xl font-bold font-IRANSansX">{title}</h2>
+
+//       <div className="flex items-center gap-2">
+//         <CTAButton
+//           label="صفحه کاربران"
+//           loadingLabel="در حال انتقال ..."
+//           path="/users"
+//         />
+//         <CTAButton
+//           label="صفحه محصولات"
+//           loadingLabel="در حال انتقال ..."
+//           path="/products"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SectionTitle;
